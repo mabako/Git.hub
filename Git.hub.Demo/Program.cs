d29497e0d8b45e86fc520d8d66ee308b9bfbe2e7
Marcus Bauer <mabako@gmail.com>
2012-05-11 01:42:29 +0200

    few demo lines

---
+++ Git.hub.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Git.hub;
+
+namespace Git.hub.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            
+            // List all repositories of the user 'mabako'
+            client.getRepositories("mabako").ToList().ForEach(repo => Console.WriteLine(repo.Name));
+
+            Console.WriteLine();
+            Console.WriteLine("Branches of zwickau-mensa?");
+            client.getRepository("mabako", "zwickau-mensa").GetBranches().ToList().ForEach(branch => Console.WriteLine("  {0} at {1}", branch.Name, branch.Commit.Sha));
+            
+            Console.ReadLine();
+        }
+    }
+}
