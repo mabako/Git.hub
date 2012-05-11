bf75eafacb3e8b9d8a78278e7911110bc2437216
Marcus Bauer <mabako@gmail.com>
2012-05-11 21:46:46 +0200

    OAuth2 Helper to use with GitHub
    Would look somewhere like the following in Windows Forms, assuming the user authorized the app.
    
            protected override void OnLoad(System.EventArgs e)
            {
                this.webBrowser1.Navigate("https://github.com/login/oauth/authorize?client_id=" + client_id + "&scope=repo,public_repo");
            }
    
            public void web_Navigating(object sender, WebBrowserNavigatingEventArgs e)
            {
                string url = e.Url.ToString();
                if(url.Contains("?code="))
                {
                    string[] splits = url.Split(new string[]{"?code="}, StringSplitOptions.RemoveEmptyEntries);
                    string code = splits[1];
                    string token = OAuth2Helper.requestToken(client_id, client_secret, code);
    
                    this.Close();
                }
            }

---
+++ Git.hub/OAuth2Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace Git.hub
+{
+    class OAuth2Data
+    {
+        public string AccessToken { get;  private set; }
+        public string TokenType { get; private set; }
+    }
+
+    public class OAuth2Helper
+    {
+        /// <summary>
+        /// Requests a Github API token, given you received a code first, i.e. user allowed that.
+        /// 
+        /// See OAuth documentation for how to do that.
+        /// Your App needs to be registered first.
+        /// </summary>
+        /// <param name="client_id">your app's client_id</param>
+        /// <param name="client_secret">your app's secret</param>
+        /// <param name="code">code you got from github</param>
+        /// <returns>oauth token if successful, null otherwise</returns>
+        public static string requestToken(string client_id, string client_secret, string code)
+        {
+            // Not on api.github.com
+            var client = new RestClient("https://github.com");
+
+            var request = new RestRequest("/login/oauth/access_token");
+            request.RequestFormat = DataFormat.Json;
+            request.AddParameter("client_id", client_id);
+            request.AddParameter("client_secret", client_secret);
+            request.AddParameter("code", code);
+
+            var response = client.Post<OAuth2Data>(request);
+            if (response.Data != null)
+                return response.Data.AccessToken;
+            return null;
+        }
+    }
+}
