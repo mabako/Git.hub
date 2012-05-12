5e6edd32fd9522eacd78a3b9d382107ff342a735
Marcus Bauer <mabako@gmail.com>
2012-05-12 15:09:19 +0200

    Commit.AuthorName, either the Github login or commit raw name <email>

--- Git.hub/Commit.cs
+++ Git.hub/Commit.cs
@@ -15,10 +15,16 @@ namespace Git.hub
     {
         public string Sha { get; private set; }
         public string Url { get; private set; }
+
+        /// <summary>
+        /// Github User, may be null if unbeknown to Github
+        /// </summary>
         public User Author { get; private set; }
         public User Committer { get; private set; }
         public List<Ref> Parents { get; private set; }
         public Commit Commit { get; private set; }
+
+        public string AuthorName { get { return Author == null ? Commit.Author.ToString() : Author.Login; } }
     }
 
     public class CommitAuthor
@@ -26,6 +32,21 @@ namespace Git.hub
         public string Name { get; private set; }
         public string Email { get; private set; }
         public DateTime Date { get; private set; }
+
+        public override string ToString()
+        {
+            if (Name != null)
+            {
+                if (Email != null)
+                    return string.Format("{0} <{1}>", Name, Email);
+                else
+                    return Name;
+            }
+            else if (Email != null)
+                return Email;
+            else
+                return "";
+        }
     }
 
     // Not too sure this is the same for normal commits.
